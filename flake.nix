30582d4eefd4e83bbc1c95468526e05f823ce51e
Kenichi Kamiya <kachick1@gmail.com>
2024-08-22 17:27:35 +0900

    Update flake style to drop flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -1,42 +1,37 @@
 {
   inputs = {
-    # Candidate channels
-    #   - https://github.com/kachick/anylang-template/issues/17
-    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
-    # How to update the revision
-    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      forAllSystems = lib.genAttrs lib.systems.flakeExposed;
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        formatter = pkgs.nixfmt-rfc-style;
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
-              # https://github.com/kachick/dotfiles/pull/228
-              bashInteractive
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-rfc-style);
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default =
+            with pkgs;
+            mkShellNoCC {
+              buildInputs = [
+                bashInteractive
+                findutils # xargs
+                nixfmt-rfc-style
+                nil
+                go-task
 
-              nil
-              nixfmt-rfc-style
-              dprint
-              typos
-              go-task
-            ];
-          };
-      }
-    );
+                dprint
+                typos
+              ];
+            };
+        }
+      );
+    };
 }
