53ff7dd901301fa660bc076788da1b9210cb71a5
Kenichi Kamiya <kachick1@gmail.com>
2026-09-25 03:44:20 +0900

    Add kachick/sh plugin

--- regex_test.ts
+++ regex_test.ts
@@ -20,6 +20,7 @@ Deno.test("Given URL matches to correct depNameTemplate", async (t) => {
     [`"https://plugins.dprint.dev/kachick/kdl-0.2.0.wasm"`, "{{{user}}}/dprint-plugin-{{{pluginName}}}"],
     [`"https://plugins.dprint.dev/kachick/typstyle-0.1.0.wasm"`, "{{{user}}}/dprint-plugin-{{{pluginName}}}"],
     [`"https://plugins.dprint.dev/kachick/nix-0.1.0.wasm"`, "{{{user}}}/dprint-plugin-{{{pluginName}}}"],
+    [`"https://plugins.dprint.dev/kachick/sh-0.1.0.wasm"`, "{{{user}}}/dprint-plugin-{{{pluginName}}}"],
   ]);
 
   for (const [url, template] of urlToTemplate) {
